16e41ad6c9a929c185b05b5d7f0a676e30cbdcd3
Graham Vasquez <rescus1221@gmail.com>
2020-01-04 17:14:09 -0500

    Changed hashing

--- server.js
+++ server.js
@@ -13,39 +13,43 @@ app.get('/', (req, res) => res.send('API Running'));
 
 app.get('/:channel_id/:referrer_id', async (req, res) => {
     try {
+        // A bit of destructuring and variable naming then creating the hash
         const { channel_id, referrer_id } = req.params;
         const useragent = req.headers['user-agent'];
         const ipAddr = await axios.get('https://api.ipify.org?format=json');
         const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(referrer_id, salt);
+        const hash = await bcrypt.hash(useragent + ipAddr, salt);
 
-        const tableData = {
-            referrer: referrer_id,
-            channel: channel_id,
-            'ip-address': ipAddr.data.ip.toString(),
-            platform: 'twitch',
-            'data-hash': hash,
-            'user-agent': useragent
-        };
+        //Compare the hash then add it to the database
+        if (bcrypt.compare(useragent + ipAddr, hash)) {
+            const tableData = {
+                referrer: referrer_id,
+                channel: channel_id,
+                'ip-address': ipAddr.data.ip.toString(),
+                platform: 'twitch',
+                'data-hash': hash,
+                'user-agent': useragent
+            };
 
-        await base('referrals').create(
-            [
-                {
-                    fields: tableData
+            await base('referrals').create(
+                [
+                    {
+                        fields: tableData
+                    }
+                ],
+                function(err, records) {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    records.forEach(function(record) {
+                        console.log(record.getId(), Date(Date.now));
+                    });
                 }
-            ],
-            function(err, records) {
-                if (err) {
-                    console.error(err);
-                    return;
-                }
-                records.forEach(function(record) {
-                    console.log(record.getId(), Date(Date.now));
-                });
-            }
-        );
+            );
 
-        res.json(tableData);
+            res.json(tableData);
+        }
     } catch (err) {
         console.error(err.message);
         res.status(500).send('An error occurred');
