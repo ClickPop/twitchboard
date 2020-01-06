53d2b6a7c0718f4db0e7994ff52ea7d7c1ec11b0
Sean Metzgar <sean.metzgar@gmail.com>
2020-01-06 17:38:38 -0500

    Update leaderboard methods

---
+++ middleware/processLeaderboard.js
@@ -0,0 +1,33 @@
+function addOrIncrementReferrer(referral, leaderboard){
+    var found = false;
+    if (leaderboard.length > 0) {
+        for (var i=0; i < leaderboard.length; i++) {
+            if (leaderboard[i].referrer === referral.referrer) {
+                leaderboard[i].views++;
+                found = true;
+            }
+        }
+    }
+    if (!found) {
+        leaderboard.push({referrer: referral.referrer, views: 1});
+    }
+
+    return leaderboard;
+
+}
+
+module.exports = async function(req, res, next = console.error) {
+    const referrals = res.locals.referrals;
+    const settings = res.locals.leaderboardSettings;
+
+    leaderboard = [];
+
+    referrals.forEach( (referral) => {
+        leaderboard = addOrIncrementReferrer(referral, leaderboard);
+    });
+
+    leaderboard.sort((a, b) => (a.views < b.views) ? 1 : -1);
+
+    res.locals.leaderboard = leaderboard;
+    next();
+};
