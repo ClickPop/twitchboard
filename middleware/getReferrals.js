cbba87f4169322f023016a34e1245d9d1b2c0625
Graham Vasquez <rescus1221@gmail.com>
2020-01-09 12:15:08 -0500

    Leaderboard page now auto-reloads if there are changes to the database

--- middleware/getReferrals.js
+++ middleware/getReferrals.js
@@ -6,28 +6,28 @@ const api_key = process.env.API_KEY;
 const base = new airtable({ apiKey: api_key }).base('appeqb1CAXVkBv8hN');
 
 module.exports = function(req, res, next) {
-    var referrals = [];
-    const channel = req.params.channel_id;
+	var referrals = [];
+	const channel = req.params.channel_id;
 
-    base('referrals')
-        .select({
-            view: 'Grid view',
-            filterByFormula: `{channel} = "${channel}"`
-        })
-        .eachPage(
-            function page(records, fetchNextPage) {
-                records.forEach(function(record) {
-                    referrals.push(record.fields);
-                });
-                fetchNextPage();
-            },
-            function done(err) {
-                if (err) {
-                    next(err);
-                }
-
-                res.locals.referrals = referrals;
-                next();
-            }
-        );
+	base('referrals')
+		.select({
+			view: 'Grid view',
+			filterByFormula: `{channel} = "${channel}"`
+		})
+		.eachPage(
+			function page(records, fetchNextPage) {
+				records.forEach(function(record) {
+					referrals.push(record.fields);
+				});
+				fetchNextPage();
+			},
+			function done(err) {
+				if (err) {
+					next(err);
+				}
+				req.referrals = referrals;
+				res.locals.referrals = referrals;
+				next();
+			}
+		);
 };
