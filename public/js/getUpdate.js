16cfb891a82f5c907bfa765f655b7662bb6c4976
Graham Vasquez <rescus1221@gmail.com>
2020-01-12 05:50:28 -0500

    Used Jquery for updating the elements of the table without reloading the page. Still needs the animation

--- public/js/getUpdate.js
+++ public/js/getUpdate.js
@@ -1,13 +1,46 @@
 const { href, protocol, hostname, port, pathname } = window.location;
-const newBoard = document.getElementById('hidden');
+var leaderboard = [];
 url = `${protocol}//${hostname}:${port}/api/v1/`;
+
+$(document).ready(() => {
+    $('table.leaderboard__table tr').each((row, tr) => {
+        if (row != 0) {
+            leaderboard[row - 1] = {
+                referrer: $(tr)
+                    .find('td:eq(0)')
+                    .text(),
+                views: $(tr)
+                    .find('td:eq(1)')
+                    .text()
+            };
+        }
+    });
+});
+
 window.setInterval(() => {
     fetch(`${url}getLeaderboard?channel=${pathname.replace('/', '')}`)
-        .then(res => {
-            if (
-                JSON.stringify(res.data.leaderboard) != newBoard.innerHTML
-            ) {
-                window.location.reload(true);
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            const newBoard = json.data.leaderboard;
+            if (newBoard != leaderboard) {
+                for (var i = 0; i < leaderboard.length; i += 1) {
+                    if (leaderboard[i] != newBoard[i]) {
+                        leaderboard[i] = newBoard[i];
+                    }
+                }
+
+                $('table.leaderboard__table tr').each((row, tr) => {
+                    if (row != 0) {
+                        $(tr)
+                            .find('td:eq(0)')
+                            .text(leaderboard[row - 1].referrer);
+                        $(tr)
+                            .find('td:eq(1)')
+                            .text(leaderboard[row - 1].views);
+                    }
+                });
             }
         });
 }, 5000);
