d63209d64bc22bcd3c23afefbcdf1a15ccd5ffc2
Graham Vasquez <rescus1221@gmail.com>
2020-01-18 23:27:43 -0500

    Added logout route and some more authentication parameters

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,10 +1,13 @@
 const passport = require('passport');
 
 module.exports = function (req, res, next) {
-    if(req.user.fields.display_name === req.params.channel) {
-        next();
+    if (req.user != undefined) {
+        if(req.user.fields.display_name === req.params.channel) {
+            next();
+        } else {
+            res.status(401).send('Unauthorized access');
+        }
     } else {
-        res.status(401).send('Unauthorized access');
+        res.redirect('/');
     }
-    // next();
 }
